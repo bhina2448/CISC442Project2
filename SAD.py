3319cd7e69122d90decde7fe09e30d26a2138026
Brielle Hina <bhina@udel.edu>
2022-11-07 05:04:03 -0500

    added SAD

--- SAD.py
+++ SAD.py
@@ -1,1 +1,31 @@
 #Region Based analysis, Sum of Absolute Differences
+import numpy as np
+
+#Gets the disparity using SAD
+#searching for template in image
+#xpos,ypos is templates position in original left side image
+def SADDisp(template, image, searchRange, xpos,ypos):
+    curr=image[xpos:xpos+template.width, ypos:ypos+template.height]
+    disp=SAD(template,curr)
+    minx=xpos-searchRange
+    maxx=xpos+searchRange
+    if(minx<0):
+        minx=0
+    if(maxx>= image.width-template.width):
+        maxx= image.width-template.width -1
+    for x in range(minx,maxx):
+        curr=image[x:x+template.width,ypos:ypos+template.height]
+        val=SAD(template,curr)
+        if(val<disp):
+            disp=val
+    return disp
+
+
+#computes SAD for arrays template and section (must be of same size)
+def SAD(template, section):
+    sum=0
+    for x in range(0,template.width):
+        for y in range(0,template.height):
+            val=abs(template[x,y]-section[x,y])
+            sum=sum+val
+    return sum
--- regionBased.py
+++ regionBased.py
@@ -5,9 +5,27 @@ import cv2 as cv
 #right- right image
 #method- either SAD, SSD, NCC
 #searchRange- size of searching area
-#templateX- x value for size of the template
-#templateY- y value for size of the template
-#disparity- disparity (horizontal motion)
-def regionBased(left,right,method,searchRange,templateX,templatY, disparity):
+#templateW- width of the template
+#templateH- height of the template
+def regionBased(left,right,method,searchRange,templateW,templateH):
     depth =np.zeros(left.shape, dtype=np.float32)
-    
+    width= (int)(left.width / templateW)*templateW
+    height=(int)(left.height/templateH)*templateH
+    for x in range(0,(width-templateW-1)):
+        for y in range(0,(height-templateH-1)):
+            curr=left[x:x+templateW, y:y+templateH]
+            disparity=getDisp(curr,right,method,searchRange,x,y)
+            depth[x,y]=disparity
+    depth=cv.normalize(depth,None, alpha=0,beta=255,norm_type=cv.NORM_MINMAX,dtype=cv.CV_8U)
+    return depth
+
+#returns the dispartiry using the given method
+#searches for the template in the given image
+def getDisp(template, image, method, searchRange, x,y):
+    if method=="SAD":
+        #use SAD
+    else if method=="SSD":
+        #use SSD
+    else:
+        #use NCC
+

ebefc0450e5b95784349383460020e76e71386f5
Brielle Hina <bhina@udel.edu>
2022-11-08 21:11:40 -0500

    added tests that reduce images for speed

--- regionBasedTest.py
+++ regionBasedTest.py
@@ -7,12 +7,12 @@ left=cv.resize(left,(0,0),fx=0.5,fy=0.5)
 r=cv.imread('venus4.png')
 right=cv.cvtColor(r, cv.COLOR_BGR2GRAY)
 right=cv.resize(right,(0,0),fx=0.5,fy=0.5)
-method="SAD"
+method="NCC"
 searchRange=75
 templateW=21
 templateH=21
 
 disparity=regionBased(left,right,method,searchRange,templateW,templateH)
 
-cv.imwrite("disparity-RB-SAD-DOWNSIZED.png", disparity)
+cv.imwrite("disparity-RB-NCC-DOWNSIZED.png", disparity)
 print("done")

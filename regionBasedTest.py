5df0ea15e438628c27f55db5d2b13e55a224d133
Brielle Hina <bhina@udel.edu>
2022-11-09 21:10:30 -0500

    ran tests with sawtooth images

--- regionBasedTest.py
+++ regionBasedTest.py
@@ -1,18 +1,20 @@
 from regionBased import *
 import cv2 as cv
 
-l=cv.imread('venus0.png')
+l=cv.imread('sawtooth0.jpg')
 left=cv.cvtColor(l, cv.COLOR_BGR2GRAY)
-left=cv.resize(left,(0,0),fx=0.5,fy=0.5)
-r=cv.imread('venus4.png')
+#left=cv.resize(left,(0,0),fx=0.5,fy=0.5)
+#left=cv.resize(left,(0,0),fx=0.5,fy=0.5)
+r=cv.imread('sawtooth6.jpg')
 right=cv.cvtColor(r, cv.COLOR_BGR2GRAY)
-right=cv.resize(right,(0,0),fx=0.5,fy=0.5)
+#right=cv.resize(right,(0,0),fx=0.5,fy=0.5)
+#right=cv.resize(right,(0,0),fx=0.5,fy=0.5)
 method="NCC"
-searchRange=75
-templateW=21
-templateH=21
+searchRange=150
+templateW=5
+templateH=5
 
 disparity=regionBased(left,right,method,searchRange,templateW,templateH)
 
-cv.imwrite("disparity-RB-NCC-DOWNSIZED.png", disparity)
+cv.imwrite("sawtooth-RB-NCC-150.png", disparity)
 print("done")
